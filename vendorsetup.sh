997a9c932bebf326ba1adec9b59e34e3b060918b
AudioGod <audiogod@sonic-developers.com>
2015-08-09 13:32:33 +0000

    SOKPify
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
-# Copyright (C) 2014 The CyanogenMod Project
+#
+# Copyright 2013 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,18 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-$(call inherit-product, device/yu/lettuce/full_lettuce.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-PRODUCT_NAME := cm_lettuce
-BOARD_VENDOR := yu
-PRODUCT_DEVICE := lettuce
-
-PRODUCT_GMS_CLIENTID_BASE := android-micromax
-
-TARGET_VENDOR_PRODUCT_NAME := YUPHORIA
-TARGET_VENDOR_DEVICE_NAME := YUPHORIA
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=YUPHORIA PRODUCT_NAME=YUPHORIA
+add_lunch_combo sokp_lettuce-userdebug
